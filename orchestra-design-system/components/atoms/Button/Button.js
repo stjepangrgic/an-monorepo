0ba735ec869d9d142e7375249f0f07717d0d0ba0
Stjepan Grgic <grgic.stjepan@gmail.com>
2020-02-26 12:04:39 +0000

    Playing with a11y addon

--- orchestra-design-system/components/atoms/Button/Button.js
+++ orchestra-design-system/components/atoms/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import StyledButton from './Button.styles'
 
-const Button = ({ children }) => {
-  return <StyledButton>{children}</StyledButton>
+const Button = ({ children, ...props }) => {
+  return <StyledButton {...props}>{children}</StyledButton>
 }
 
 export default Button
--- orchestra-design-system/components/atoms/Button/Button.stories.js
+++ orchestra-design-system/components/atoms/Button/Button.stories.js
@@ -1,10 +1,19 @@
 import React from 'react'
 // import { action } from '@storybook/addon-actions'
 import Button from './Button'
+import { withA11y } from '@storybook/addon-a11y'
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withA11y],
 }
 
 export const Default = () => <Button>Test Button</Button>
+export const Accesible = () => <Button role="button">Accesible Button</Button>
+export const InputButton = () => <input type="button" value="Input Button" />
+export const Inacessible = () => (
+  <button style={{ backgroundColor: 'red', color: 'darkRed' }}>
+    Inaccessible button
+  </button>
+)
